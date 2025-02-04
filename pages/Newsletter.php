868d427bbde5fbb6b4094039161ff87e3068801a
Diarrisso <diarrisso49@gmail.com>
2025-02-04 20:03:04 +0100

    delete files

--- pages/Newsletter.php
+++
@@ -1,8 +0,0 @@
-<section class='newsletter_block' id='newsletter'>
-    <div class='newsletter_block_flex'>
-        <span>Newsletter</span>
-        <h2>Nichts verpassen!</h2>
-        <div>At wero iquam erat volutpat . Nullam scelerisque auctor libero</div>
-        <button>Newsletter abonnieren</button>
-    </div>
-</section>
