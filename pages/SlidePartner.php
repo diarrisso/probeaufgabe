648c2e9d356deeccaeeb291457e6ff5581fa465c
Diarrisso <diarrisso49@gmail.com>
2025-02-04 19:59:21 +0100

    update project with React

--- pages/SlidePartner.php
+++
@@ -1,9 +0,0 @@
-<section class='slide_partner' id='slide'>
-    <div class='intro-slider swiper'>
-        <div class='swiper-wrapper'>
-        </div>
-        <div class='swiper-pagination'></div>
-        <div class='swiper-button-next'></div>
-        <div class='swiper-button-prev'></div>
-    </div>
-</section>
